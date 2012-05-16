ead2758865b1e12849410e0160461183cacea4f3
SvartalF <self@svartalf.info>
2012-05-16 21:48:02 +0900

    Class mapping for `struct quirc;`

--- quirc/__init__.py
+++ quirc/__init__.py
@@ -13,3 +13,11 @@ libquirc = ctypes.CDLL(find_library('quirc'), use_errno=True)
 version = libquirc.quirc_version
 version.restype = ctypes.c_char_p
 version.__doc__ = '''Obtain the library version string.'''
+
+
+class Quirc(ctypes.Structure):
+    """struct quirc;"""
+
+    pass
+
+QuircPointer = ctypes.POINTER(Quirc)
