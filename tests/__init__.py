7c05f3803f1c29279f0fd6f2ac277ecba6c1ee6a
SvartalF <self@svartalf.info>
2012-05-18 09:52:09 +0900

    Using unittest2 for tests under Python<2.7

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*_
 
 import os.path
-import unittest
+
+try:
+    # For Python<2.7 use unitest2 for testing
+    import unittest2 as unittest
+except ImportError:
+    import unittest
 
 def load_tests():
     return unittest.TestLoader().discover(os.path.dirname(__file__))
