a27180dabc31c0f7843602ea69070884e99c860a
SvartalF <self@svartalf.info>
2012-05-23 22:02:52 +0900

    License file is not included in the setup.py, because of path problems. Doh.

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
 
+"""
+python-quirc
+============
+
+python-quirc is a ctypes bindings for `quirc <https://github.com/dlbeer/quirc/>`_, library for decoding QR codes.
+
+This library is fast, small, easy to use, have a very small memory usage,
+and optionally requires one of the image processing libraries.
+"""
+
 from setuptools import setup
 
 setup(
     name='quirc',
     version='0.6.1',
-    license=open('LICENSE').read(),
     author='SvartalF',
     author_email='self@svartalf.info',
     url='https://github.com/svartalf/python-quirc',
     description='ctypes wrapper for QR code decoding library `libquirc`',
+    long_description=__doc__,
     packages=('quirc',),
     test_suite='tests.load_tests',
     classifiers=(
